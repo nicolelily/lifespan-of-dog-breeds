2d7543da6db040236eef17d3b84629ad0b34d1c5
Nicole Mark <nicolemark@gmail.com>
2022-11-09 13:07:31 -0500

    Updated viridis ridgeline plot

--- Ridgeline Plot_w_ECDF.R
+++ Ridgeline Plot_w_ECDF.R
@@ -3,8 +3,7 @@ library(ggridges)
 library(viridis)
 library(hrbrthemes)
 
-
-p <- ggplot(dog_data_r, aes(x = dog_data_r$Lifespan, y=dog_data_r$`Breed Group UKC`,
+  ggplot(dog_data_r, aes(x = dog_data_r$Lifespan, y=dog_data_r$`Breed Group UKC`,
   fill = 0.5 - abs(0.5 - stat(ecdf)))) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE) +
   scale_fill_viridis_c(name = "Tail probability", direction = -1) +
